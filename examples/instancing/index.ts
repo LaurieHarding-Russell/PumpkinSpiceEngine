8d391a5a85ff922adb27b7cfd7b87a5412f53644
Laurie Harding-Russell <laurieHardingRussell@gmail.com>
2025-08-16 08:03:41 -0600

    Half made stuff

---
+++ examples/instancing/index.ts
@@ -0,0 +1,112 @@
+import { Renderer } from '@pumkinspicegames/pumpkinSpiceEngine/renderer';
+import { Vector3 } from "@pumkinspicegames/pumpkinSpiceEngine/model/vector";
+import { BufferFactory, tileModalReference } from "@pumkinspicegames/pumpkinSpiceEngine/buffer-factory";
+import { ShadersType } from "@pumkinspicegames/pumpkinSpiceEngine/model/model-reference";
+import { ModelResources } from './models';
+
+const maxZoom = -1;
+const minZoom = -20;
+
+var map: Array<string> = ["1"];
+var selectedType = '0';
+
+
+var renderer: Renderer;
+let position:Vector3 = {
+    x: 0,
+    y: 0,
+    z: 0
+}
+let camera = {
+    position: {
+        x: 0,
+        y: 0,
+        z: -20
+    }, 
+    rotation: {
+        x: 0,
+        y: 0,
+        z: 0
+    }
+}
+
+// FIXME, time hack
+setTimeout(() => {
+
+    initializeMovementListeners();
+
+    const gameWindow: HTMLCanvasElement | null = document.querySelector("#screen")    
+
+    if (gameWindow == null) {
+        throw "couldn't find things and magic and stuff";
+    }
+
+    const webGl: WebGL2RenderingContext | null = gameWindow.getContext("webgl2");
+    if (webGl == null) throw "oh no! uggg";
+
+    let modelResources = new ModelResources();
+    modelResources.load().then(() => {
+      let bufferFactory = new BufferFactory();
+      bufferFactory
+        .addModel("Cube", modelResources.cube, ShadersType.main)
+        .defaultSkin = modelResources.defaultSkin;
+
+        renderer = new Renderer(webGl);
+        renderer.start(bufferFactory).then(() => {
+            // FIXME, loading
+            setInterval(() => {
+                gameWindow.width = window.innerWidth
+                gameWindow.height = window.innerHeight
+
+                renderer.setProjectionMatrix(camera);
+
+                renderer?.renderMain(position, 
+                    bufferFactory.modelReferences.get("Cube")!
+                );
+            }, 33)
+        });
+    });
+}, 1000);
+
+function initializeMovementListeners() {
+    document.addEventListener('keydown', (event: KeyboardEvent) => {
+
+        if (event.key == '8') {
+            camera.rotation.x += 0.1;
+        }
+
+        if (event.key == '2') {
+            camera.rotation.x -= 0.1;
+        }
+
+        if (event.key == '4') {
+            camera.rotation.z -= 0.1;
+        }
+
+        if (event.key == '6') {
+            camera.rotation.z += 0.1;
+        }
+
+        if (event.key == 'a') {
+            camera.position.x += 0.1;
+        }
+
+        if (event.key == 'd') {
+            camera.position.x -= 0.1;
+        }
+
+        if (event.key == 'w') {
+            camera.position.y -= 0.1;
+        }
+
+        if (event.key == 's') {
+            camera.position.y += 0.1;
+        }
+    }, false);
+    
+    document.onwheel = (event: WheelEvent) => {
+        event.preventDefault();
+        camera.position.z += event.deltaY * -0.01;
+        camera.position.z = Math.min(Math.max(minZoom, camera.position.z), 0);
+    }
+}
---
+++ examples/instancing/models.ts
@@ -0,0 +1,60 @@
+import { ModelInfo } from "@pumkinspicegames/pumpkinSpiceEngine/model-info";
+import { loadPSObject } from "@pumkinspicegames/pumpkinSpiceEngine/model-loaders/ps.loader";
+
+export class ModelResources {
+    
+
+    cube!: ModelInfo;
+    defaultSkin!: TexImageSource
+    
+    constructor() {
+    }
+
+    public async load() {
+        await this.loadCube();
+    }
+
+    public async loadModel(name: string, skin: TexImageSource): Promise<ModelInfo> {
+        let result = await this.makeRequest("GET", name);
+        return loadPSObject(result, skin);
+    }
+
+    public async loadSkin(name: string): Promise<TexImageSource> {
+        const resultSkin = new Image();
+        resultSkin.src = name;
+        await resultSkin.decode();
+        return resultSkin;
+    }
+    
+
+    private async loadCube() {
+        const resultSkin = await this.loadSkin("cube.png");
+        this.cube = await this.loadModel("cube.ps", resultSkin);
+        this.defaultSkin = await this.loadSkin("cube.png");
+    }
+
+    private makeRequest(method: string, url: string): Promise<string> {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open(method, url);
+            request.onload = function () {
+                if (this.status >= 200 && this.status < 300) {
+                    resolve(request.responseText);
+                } else {
+                    reject({
+                        status: this.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
+            request.onerror = function () {
+                reject({
+                    status: this.status,
+                    statusText: request.statusText
+                });
+            };
+            request.send();
+        });
+    }
+}
+
